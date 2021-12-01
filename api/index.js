86bfd6fd84548856c1d8a1391637e70e3e394bc7
Arinzechukwu Edmund <edmund10arinze@gmail.com>
2021-12-01 21:27:17 +0100

    netflix_clonet

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => console.log("mongodb connection error"));
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.listen(8800, () => {
+  console.log("backend server is running");
+});
+
